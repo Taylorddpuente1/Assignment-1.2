68f61bedebb051659833bb0a2c9ed65c8d53f175
Taylo <Taylo@MARIEL>
2025-04-09 22:31:23 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿
+ // Ask user to Input first number
+Console.Write("input 1st number");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+//Ask user to Input second number
+Console.Write("input 2nd number");
+int num2 = Convert.ToInt32(Console.ReadLine());
+if (num1 == num2);
+{
+    Console.WriteLine($"{num1} and {num2} are equal");
+}
+
+Console.WriteLine($"{num1} and {num2} are not equal");
+
+
+
+
+
+
